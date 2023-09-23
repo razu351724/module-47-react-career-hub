2817617967b7854e1a76eb28fe9fdfb13a0dcc86
RAZU-sheikh <razu351724@gmail.com>
2023-09-24 04:10:36 +0600

    <feat>: The CategoryList compo create and Home component a add body: jokon home page takba tokon jobcategory list and reaturde jobs show korba

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,10 @@
+const CategoryList = () => {
+    return (
+        <div>
+            <h3 className="text-6xl text-center">Job Category List</h3>
+            <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+    );
+};
+
+export default CategoryList;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <CategoryList></CategoryList>
             <h2>Home components</h2>
         </div>
     );
