13648b1158b5f6c15ca3424f0a52e0dbd3800807
RAZU-sheikh <razu351724@gmail.com>
2023-09-24 02:51:49 +0600

    <feat>: The Header compo create and Root compo add commit-body: deseui taka naveber in at arun a head component

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Root = () => {
     return (
         <div>
-            <h3>Root vi</h3>
+            <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
