9de6f2c13dc43fab86700361c7ceba736c847334
RAZU-sheikh <razu351724@gmail.com>
2023-09-24 02:16:43 +0600

    <feat>: The  Footer.jsx compo create  and Root.jsx cold

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <h3>Root vi</h3>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
